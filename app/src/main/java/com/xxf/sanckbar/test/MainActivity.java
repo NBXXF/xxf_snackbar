34886dc2df6f0d27f37c5a8261f71a3149671514
XXF <xuanyouwu@163.com>
2020-08-31 11:11:59 +0800

    Initial commit

---
+++ app/src/main/java/com/xxf/sanckbar/test/MainActivity.java
@@ -0,0 +1,26 @@
+package com.xxf.sanckbar.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.xxf.view.sanckbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Snackbar
+                .make(getWindow().getDecorView(), "Had a snack at Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("Action Button", "onClick triggered");
+                    }
+                }).show();
+    }
+}
